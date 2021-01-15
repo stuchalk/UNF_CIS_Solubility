8d3ae50c1aa9d09d9e4e6b1ffab35026e261945d
stuchalk <schalk@unf.edu>
2021-01-15 13:57:31 -0500

    requirements update

--- reports/tests.py
+++ reports/tests.py
@@ -1,19 +1,20 @@
 """ django unit test file"""
 import os
 import django
+import json
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings")
 django.setup()
 
 from reports.serializers import *
 from scidata.model import *
-
-sysid = '58_2'
+from datetime import datetime
+sysid = '58_1'
 rep = Reports.objects.get(sysid__exact=sysid)
 report = ReportSerializer(rep)
 data = report.data
-print(json.dumps(data, indent=4))
-exit()
+# print(json.dumps(data, indent=4))
+# exit()
 
 # organize data
 pub = data['pub']
@@ -26,14 +27,19 @@ test = SciData(sysid)
 test.context(['https://stuchalk.github.io/scidata/contexts/sds.jsonld',
               'https://stuchalk.github.io/scidata/contexts/scidata.jsonld'])
 test.base({"@base": "https://scidata.unf.edu/iupac/sds/" + sysid + "/"})
+test.version(1)
+test.generatedAt(str(datetime.now()))
 test.publisher('The International Union of Pure and Applied Chemistry')
 test.add_keyword('Solubility')
 test.add_keyword('Solubility data series')
 test.title('Solubility data from volume ' + pub['volume'])
+
+
+
 test.add_source([{"title": pub['title'], "year": pub['year'], "type": "Critically evaluate report"}])
 if ref:
     test.add_source([{"title": ref['title'], "year": ref['year'], "type": "Journal article", "doi": ref['doi']}])
-
+test.rights("https://creativecommons.org/licenses/by-nc/4.0/", "NIST & IUPAC")
 output = test.output
 
-json.dumps(output)
+print(json.dumps(output, indent=4))
