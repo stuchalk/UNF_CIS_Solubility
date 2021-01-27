d5b2e1ebd5afaeba812409b3dcb512343942bef3
stuchalk <schalk@unf.edu>
2021-01-27 11:51:49 -0500

    updates 1/27/21

--- reports/tests.py
+++ reports/tests.py
@@ -13,8 +13,8 @@ sysid = '58_2'
 rep = Reports.objects.get(sysid__exact=sysid)
 report = ReportSerializer(rep)
 data = report.data
-print(json.dumps(data, indent=4))
-exit()
+# print(json.dumps(data, indent=4))
+# exit()
 
 # organize data
 pub = data['pub']
@@ -30,24 +30,39 @@ test.add_namespace({'w3i': 'https://w3id.org/skgo/modsci#'})
 test.base({"@base": "https://scidata.unf.edu/iupac/sds/" + sysid + "/"})
 test.version(1)
 test.generatedAt(str(datetime.now()))
+
 # add general metadata
 test.title('Solubility data from volume ' + pub['volume'])
-#
+austr = data['set'][0]['reference']['authors']
+aulist = austr.split("; ")
+aus = []
+for au in aulist:
+    aus.append({'name': au})
+test.author(aus)
 
-
-test.author(data["set"][0])
-# string = "Stuart Chalk"
-# parts = string.split(" ")
 test.publisher('The International Union of Pure and Applied Chemistry')
 test.add_keyword('Solubility')
 test.add_keyword('Solubility data series')
 test.discipline('w3i:Chemistry')
 test.subdiscipline('w3i:PhysicalChemistry')
 
+# SciData section
+
+# methodology
+
+# system
+
+# dataset
+
+# sources
 test.add_source([{"title": pub['title'], "year": pub['year'], "type": "Critically evaluate report"}])
 if ref:
     test.add_source([{"title": ref['title'], "year": ref['year'], "type": "Journal article", "doi": ref['doi']}])
+
+# rights
 test.rights("https://creativecommons.org/licenses/by-nc/4.0/", "NIST & IUPAC")
+
+# generate JSON-LD
 output = test.output
 
 print(json.dumps(output, indent=4))
