1179b9ea110ca4e63eeb38dffc38ed0530236c91
Stuart Chalk <schalk@unf.edu>
2022-03-29 13:53:21 -0400

    updates to datasets

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from sds.models import Datasets
+from sds.models import Dataseries
+from sds.models import Datapoints
+from sds.models import Conditions
+from sds.models import Data
 
-# Register your models here.
+
+@admin.register(Datasets)
+class DatasetsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'report')
+    ordering = ('title', 'report')
+    search_fields = ('title', 'description')
--- sds/models.py
+++ sds/models.py
@@ -158,14 +158,11 @@ class Dataseries(models.Model):
 class Datasets(models.Model):
     """ datasets table model """
     id = models.AutoField(primary_key=True)
-    sysid = models.CharField(max_length=10)
-    sysnmid = models.PositiveIntegerField()
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
     report = models.ForeignKey("Reports", models.DO_NOTHING, db_column="report_id")
-    reference = models.ForeignKey("References", models.DO_NOTHING, db_column="reference_id")
-    system = models.ForeignKey("Systems", models.DO_NOTHING, db_column="system_id")
-    eval = models.IntegerField(blank=True, null=True)
     comments = models.CharField(max_length=256, blank=True, null=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField()
 
     class Meta:
         managed = False
