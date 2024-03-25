45ee450100c1a45d9a3fbd27a95c60cedf7600fd
Stuart Chalk <schalk@unf.edu>
2024-03-25 15:03:56 -0400

    updated comments in urls.py in solubility

--- solubility/urls.py
+++ solubility/urls.py
@@ -1,8 +1,8 @@
-""" urls for the substances app """
+""" urls for the solubility app """
 from django.urls import path
 from . import views
 
-
+# this is for the home page...
 urlpatterns = [
     path("", views.index, name='index'),
 ]
