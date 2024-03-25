388205451b849a6957ebe4cad7fc9923b33c34dd
Stuart Chalk <schalk@unf.edu>
2024-03-25 14:53:45 -0400

    updates to fix references views template updates

--- references/views.py
+++ references/views.py
@@ -5,15 +5,15 @@ from sds.models import *
 
 def index(request):
     """ present an overview page about the references in the sds"""
-    data = References.objects.all().values('id', 'raw')
+    data = References.objects.all().values('id', 'citation')
     chars = []
     refs = {}
     for r in data:
-        first = r['raw'][0].upper()
+        first = r['citation'][0].upper()
         if first not in refs.keys():
             refs.update({first: []})
             chars.append(first)
-        refs[first].append(tuple((r['id'], r['raw'])))
+        refs[first].append(tuple((r['id'], r['citation'])))
     return render(request, "../templates/references/index.html", {'refs': refs, 'chars': chars})
 
 
@@ -21,9 +21,7 @@ def view(request, refid=0):
     """ show data about a specific reference"""
     ref = References.objects.get(id=refid)
     rrptids = ReferencesReports.objects.all().filter(reference_id=refid).values_list('report_id', flat=True)
-    rdsets = Datasets.objects.all().filter(report_id__in=rrptids).values('report_id', 'system__name')
-    evalids = EvaluationsReferences.objects.all().filter(reference_id=refid).values_list('evaluation_id', flat=True)
-    erptids = Evaluations.objects.all().filter(id__in=evalids).values_list('report_id', flat=True)
-    edsets = Datasets.objects.all().filter(id__in=erptids).values('report_id', 'system__name')
+    rdsets = Reports.objects.all().filter(id__in=rrptids, type='compilation').values('id', 'system__name')
+    edsets = Reports.objects.all().filter(id__in=rrptids, type='evaluation').values('id', 'system__name')
 
     return render(request, "../templates/references/view.html", {'ref': ref, 'evals': edsets, 'dsets': rdsets})
