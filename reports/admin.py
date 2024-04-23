85df8fbb838779cca10de50663fc651140862bbe
Stuart Chalk <schalk@unf.edu>
2024-04-23 08:51:33 -0400

    add reports id

--- reports/admin.py
+++ reports/admin.py
@@ -4,7 +4,7 @@ from sds.models import Reports
 
 @admin.register(Reports)
 class ReportsAdmin(admin.ModelAdmin):
-    list_display = ('volume', 'system', 'type', 'page')
+    list_display = ('id', 'volume', 'system', 'type', 'page')
     ordering = ('volume__vol', 'system__name', 'page')
     search_fields = ('volume__vol', 'page', 'system__name')
     list_filter = ('volume__vol',)
