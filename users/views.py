dfe2ddce10f62a2eb72722b9c40f33004d3a6088
Stuart Chalk <schalk@unf.edu>
2024-04-24 09:41:08 -0400

    add data total per student to users webpage

--- users/views.py
+++ users/views.py
@@ -9,6 +9,14 @@ def index(request):
     """present an overview page about the substance in the sds"""
     usrs = (User.objects.all().filter(last_login__gt='2024-03-01').
             values('id', 'first_name', 'last_name').order_by('last_name'))
+    for usr in usrs:
+        ccnt = LogEntry.objects.all().filter(user_id=usr['id'], content_type_id=16, action_flag=1).count()
+        dcnt = LogEntry.objects.all().filter(user_id=usr['id'], content_type_id=17, action_flag=1).count()
+        scnt = LogEntry.objects.all().filter(user_id=usr['id'], content_type_id=33, action_flag=1).count()
+        usr.update({'concnt': ccnt})
+        usr.update({'datcnt': dcnt})
+        usr.update({'supcnt': scnt})
+        usr.update({'total': ccnt + dcnt + scnt})
     return render(request, "../templates/users/index.html", {'usrs': usrs})
 
 
